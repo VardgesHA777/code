a7cb50c0c38dfd95d2d09aea4290364029e6d801
Vardges <vardgeshyan@gmail.com>
2023-08-05 23:55:30 +0400

    sss

--- src/pages/Users/Users.jsx
+++ src/pages/Users/Users.jsx
@@ -1,47 +1,20 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Table, Pagination } from 'antd';
 
-import {getAllUsers} from "../../requests/reqeusts";
-
-import { usersColumnNames } from "../../constants/usersColumnNames";
+import LeafletRuler from "../../components/LeafletRuler";
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 const Users = () => {
-  const navigate = useNavigate()
-  const [page, setPage] = useState(1)
-  const [usersData, setUsersData] = useState()
-  const [loading,setLoading] = useState()
-
-  useEffect(() => {
-    (async () => {
-      setLoading(true)
-      const response = await getAllUsers({start: page})
-      setUsersData(response.data.map(user => {
-        const {name, username, email, address, id } = user
-        const { street, suite, city, zipcode } = address
-        return {
-          fullName: name + ' ' + username,
-          email,
-          address: [street, suite, city, zipcode].join(', '),
-          id: id
-        }
-      }))
-      setLoading(false)
-    })()
-  },[page])
+    const position = [51.505, -0.09];
 
   return (
     <div>
-      Users
-      {loading ? <div>Loading</div> : <><Table  onRow={(record, rowIndex) => {
-        return {
-          onClick: () => {
-            navigate(`/invite-new-user/${record.id}`)
-          },
-        };
-      }} columns={usersColumnNames} dataSource={usersData} pagination={false}/>
-        <Pagination className='user-table-pagination' defaultCurrent={page} total={50} onChange={(page, pageSize) => setPage(page)}/>
-      </>}
+        <MapContainer center={position} zoom={13} style={{ height: "100vh" }}>
+            <TileLayer
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <LeafletRuler />
+        </MapContainer>
     </div>
   );
 };
